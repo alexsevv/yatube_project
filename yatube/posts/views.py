604a3156c9a6ae52905876989c9e650a8825c937
Александр Матияка <matiacka@yandex.ru>
2022-10-16 22:18:23 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главнейшая страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f"""здесь будут определенной группы. А это имя
+                        текущей группы: {slug}"""
+                        )
