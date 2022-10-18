861ed54a880d679171d0ca1bfcaf802468cfa1f2
Александр Матияка <matiacka@yandex.ru>
2022-10-18 15:43:35 +0300

    after 3/11 4/7 3/6 render views context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,24 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главнейшая страница')
+    template = 'posts/index.html'
+    title = "Последние обновления на сайте"
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f"""здесь будет название определенной группы. А это имя
-                        текущей группы: {slug}"""
-                        )
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = "Лев Толстой – зеркало русской революции."
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
