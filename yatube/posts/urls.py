95ef506bcefeca7d6f7ec81f7c22906b81124582
Александр Матияка <matiacka@yandex.ru>
2022-10-18 22:23:48 +0300

    3/11 4/7 4/6 namespace

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
-    path('group_list.html', views.group_list),
+    path('', views.index, name="index_posts"),
+    path('group_list.html', views.group_list, name="group_list"),
 ]
