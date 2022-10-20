eba43da9ef2fa1229ed7e97052ba00ea9ae33d9a
Александр Матияка <matiacka@yandex.ru>
2022-10-20 12:44:10 +0300

    3/11 5/7 6/6 add model Group

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,3 +21,10 @@ class Post(models.Model):
         on_delete=models.CASCADE,  # если удалили юзера и посты его удалятся
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='group_posts',
+        blank=True,
+        null=True
+        )
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,5 +7,5 @@ app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index, name="index_posts"),
-    path('group_list.html', views.group_list, name="group_list"),
+    path("group/<slug:slug>/", views.group_posts, name="group_slug"),
 ]
